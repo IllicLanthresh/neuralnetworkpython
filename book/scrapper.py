97eadcf265b44283a5efec3957db5d2a0fac4076
Illic <miqueldreamer@gmail.com>
2019-09-12 14:23:23 +0200

    fdgsdfg

--- book/scrapper.py
+++ book/scrapper.py
@@ -2,8 +2,7 @@ import requests as re
 from lxml import html
 
 baseurl = "http://neuralnetworksanddeeplearning.com/"
-page = re.get(baseurl)
-parsed = html.fromstring(page.content)
+page = html.fromstring(re.get(baseurl).content)
 
-for a in parsed.xpath('//*[@id="toc"]/p[@class="toc_title"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_not_mainchapter"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_mainchapter"]/a[not(@id)]'):
+for a in page.xpath('//*[@id="toc"]/p[@class="toc_title"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_not_mainchapter"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_mainchapter"]/a[not(@id)]'):
     print(baseurl + a.get("href") + " - " + a.text)
