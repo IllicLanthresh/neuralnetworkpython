eba9def2e02f0bc974ebf6a6fcad717449d13f52
Illic <miqueldreamer@gmail.com>
2019-09-12 14:20:18 +0200

    fgsdfg

--- book/scrapper.py
+++ book/scrapper.py
@@ -6,4 +6,4 @@ page = re.get(baseurl)
 parsed = html.fromstring(page.content)
 
 for a in parsed.xpath('//*[@id="toc"]/p[@class="toc_title"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_not_mainchapter"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_mainchapter"]/a[not(@id)]'):
-    print(a.text + " - " + baseurl + a.get("href"))
+    print(baseurl + a.get("href") + " - " + a.text)
