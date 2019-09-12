fc53481789d79073b075d999593d1df786c92d1f
Illic <miqueldreamer@gmail.com>
2019-09-12 14:18:38 +0200

    added scrapper

---
+++ book/scrapper.py
@@ -0,0 +1,9 @@
+import requests as re
+from lxml import html
+
+baseurl = "http://neuralnetworksanddeeplearning.com/"
+page = re.get(baseurl)
+parsed = html.fromstring(page.content)
+
+for a in parsed.xpath('//*[@id="toc"]/p[@class="toc_title"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_not_mainchapter"]/a[not(@id)] | //*[@id="toc"]/p[@class="toc_mainchapter"]/a[not(@id)]'):
+    print(a.text + " - " + a.get("href"))
