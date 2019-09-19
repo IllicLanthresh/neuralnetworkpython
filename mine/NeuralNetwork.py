bc5053e90e505c347fe8eb365566fc28c8824ad3
Illic <miqueldreamer@gmail.com>
2019-09-19 19:08:16 +0200

    stable changes

--- mine/NeuralNetwork.py
+++ mine/NeuralNetwork.py
@@ -4,13 +4,14 @@ import random
 class NeuralNetwork:
 
     def __init__(self, layer_sizes):
+        self.num_layers = len(layer_sizes)
         weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:], layer_sizes[:-1])]
         self.weights = [np.random.standard_normal(shape)/shape[1]**0.5 for shape in weight_shapes]
         self.biases = [np.zeros((size,1)) for size in layer_sizes[1:]]
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = self.sigmoid(np.dot(w, a)+b)
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
@@ -49,10 +50,10 @@ class NeuralNetwork:
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
-            activation = sigmoid(z)
+            activation = self.sigmoid(z)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -63,19 +64,28 @@ class NeuralNetwork:
         # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = sigmoid_prime(z)
+            sp = self.sigmoid_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
-    def cost_derivative(self, output_activations, y):
-        return (output_activations-y)
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
     
+    @staticmethod
+    def cost_derivative(output_activations, y):
+        return (output_activations-y)
+
     @staticmethod
     def sigmoid(x):
         return 1/(1+np.exp(-x))
 
     @staticmethod
     def sigmoid_prime(z):
-        return sigmoid(z)*(1-sigmoid(z))
+        return NeuralNetwork.sigmoid(z)*(1-NeuralNetwork.sigmoid(z))
